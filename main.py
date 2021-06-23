dc83f4d3bba9896f39b2a4929493b29034dd0995
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-23 21:45:59 +1000

    json parsing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-OWM_Endpoint= "https://api.openweathermap.org/data/2.5/onecall?"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall?"
 api_key = "bc652ccdd33dd01f43f161c59197514e"
 
 weather_param = {
@@ -13,5 +13,16 @@ weather_param = {
 response = requests.get(OWM_Endpoint, params=weather_param)
 response.raise_for_status()
 weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
 
-print(weather_data)
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain == True:
+    print("Bring an umbrella")
+
+# print(weather_data["hourly"][0]["weather"])
